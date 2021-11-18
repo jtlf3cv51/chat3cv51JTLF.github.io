ce44b9d08b042254c151805636dc18b8310cf7d1
jtlf3cv51 <jtlf3cv51@gmail.com>
2021-11-18 14:30:17 -0600

    CambiosProyecto chat

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDz0SS-oxM4z9QQ0dPpvzx8c-1f_4jp7HI",
+  authDomain: "chat3cv51jtlf.firebaseapp.com",
+  projectId: "chat3cv51jtlf",
+  storageBucket: "chat3cv51jtlf.appspot.com",
+  messagingSenderId: "330320543800",
+  appId: "1:330320543800:web:057b0220043c71f12666cd",
+  measurementId: "G-6L44FT7T5J"
+    });
