7f2bb1b669a1778fc3cc5d1b8840630ec225e4e9
jtlf3cv51 <jtfl3cv51@gmail.com>
2021-11-20 16:47:37 -0600

    BdCambios

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDz0SS-oxM4z9QQ0dPpvzx8c-1f_4jp7HI",
+  authDomain: "chat3cv51jtlf.firebaseapp.com",
+  projectId: "chat3cv51jtlf",
+  storageBucket: "chat3cv51jtlf.appspot.com",
+  messagingSenderId: "330320543800",
+  appId: "1:330320543800:web:057b0220043c71f12666cd",
+  measurementId: "G-6L44FT7T5J"
+});
